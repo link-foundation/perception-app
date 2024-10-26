b5ad53349eda4a68a9806f338be63ae6d9a2982c
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-10-26 18:46:28 +0000

    0.0.108

--- pages/api/preload.ts
+++ pages/api/preload.ts
@@ -1,7 +1,7 @@
 import { preloadApi } from '@deep-foundation/perception-imports';
 let initial = false;
 console.log('process?.env', process?.env);
-const preloadHandler = await preloadApi(process.env.GQL);
+const preloadHandler = await preloadApi(process.env.GQL, process.env.SECRET, null, false);
 export default async function(req, res) {
     return await preloadHandler(req, res);
 }
